83e6925446eae9272293d84118d01842126546a3
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-25 08:54:33 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import Tabs from './Tabs';
-import Viewport from './Viewport';
 
-// Style files
-import '../styles/Tab.css';
-import '../styles/Tabs.css';
-import '../styles/Viewport.css';
-
-const App = () => {
-    return (
-        <div>
-            <Tabs />
-            <Viewport />
-        </div>
-    );
+function App() {
+  return (
+    <div className="App">
+      <Tabs />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import Tab from './Tab';
+import React, { useState } from "react";
 
-const Tabs = () => {
-    return (
-        <div className='tabs'>
-            <Tab
-                name="Home"
-            />
-            <Tab
-                name="About"
-            />
-            <Tab
-                name="Features"
-            />
-        </div>
-    )
+function Tabs() {
+  const [activeTab, setActiveTab] = useState("Home");
+
+  const handleTabClick = (tabName) => {
+    setActiveTab(tabName);
+  };
+
+  return (
+    <div className="tabs">
+      <div className="tab" id="Home-tab" onClick={() => handleTabClick("Home")}>
+        Home
+      </div>
+      <div className="tab" id="About-tab" onClick={() => handleTabClick("About")}>
+        About
+      </div>
+      <div className="tab" id="Features-tab" onClick={() => handleTabClick("Features")}>
+        Features
+      </div>
+      <div className="viewport">
+        Pages Go Here
+        {activeTab === "Home" && <p>Home content goes here</p>}
+        {activeTab === "About" && <p>About content goes here</p>}
+        {activeTab === "Features" && <p>Features content goes here</p>}
+      </div>
+    </div>
+  );
 }
 
 export default Tabs;
