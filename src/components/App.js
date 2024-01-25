f0e8cb51848de8e416743cb4a079194a24ae9363
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-25 08:31:34 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,17 @@
+import React from "react";
+import Tabs from "./Tabs";
 
+const App=()=>{
+    return(
+        <div>
+            <div className="viewport">Pages Go Here</div>
+            <div className="tabs">
+                <div className="tab" id="Home-Tab">Home</div>
+                <div className="tab" id="About-Tab">About</div>
+                <div className="tab" id="Features-Tab">Features</div>
+            </div>
+        </div>
+    )
+}
+
+export default App
