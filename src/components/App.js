ce01b62250a6e441d2758d178296d6806953a070
misterKumar <120304503+misterKumar@users.noreply.github.com>
2024-01-25 08:41:05 +0530

    check

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,16 @@
-import React, { useState } from 'react';
-import '../styles/App.css';
+
+import React from 'react';
+import ReactDOM from "react-dom";
+import TabComponent from './TabComponent';
 
 
 
-const TABS = [
-  { id: 'Home', label: 'Home' },
-  { id: 'About', label: 'About' },
-  { id: 'Features', label: 'Features' },
-];
-
-const App = () => {
-  const [activeTab, setActiveTab] = useState(TABS[0].id);
-
-  const handleTabClick = (tabId) => {
-    setActiveTab(tabId);
-  };
-
-  return (
-    <div className="App">
-      <div className="tabs">
-        {TABS.map((tab) => (
-          <div
-            key={tab.id}
-            className={`tab ${activeTab === tab.id ? 'active' : ''}`}
-            onClick={() => handleTabClick(tab.id)}
-          >
-            {tab.label}
-          </div>
-        ))}
-      </div>
-      <div className="viewport">
-        Pages Go Here
-      </div>
-    </div>
-  );
-};
+const App=()=>{
+    return(
+        <div>
+            <TabComponent/>
+        </div>
+    )
+}
 
 export default App;
